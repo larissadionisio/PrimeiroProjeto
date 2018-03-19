54313f4ad48159bfe0401f03a1eb6c234a6c482b
larissadionisio <larissadferrari@outlook.com>
2018-03-19 12:24:15 -0300

    ListaDinamica, fase1

---
+++ ListaDinamica/app/src/main/java/com/example/alunos/listadinamica/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.alunos.listadinamica;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void mostrarLista(View v) {
+        Intent it = new Intent(this, mostrarListaDinamica.class);
+        startActivity(it);
+    }
+}
---
+++ ListaDinamica/app/src/main/java/com/example/alunos/listadinamica/mostrarListaDinamica.java
@@ -0,0 +1,8 @@
+package com.example.alunos.listadinamica;
+
+/**
+ * Created by alunos on 19/03/18.
+ */
+
+public class mostrarListaDinamica {
+}
