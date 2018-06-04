7bd57df4483eda54b21d613aeedf8726f3dccb80
larissadionisio <larissadferrari@outlook.com>
2018-06-04 12:09:12 -0300

    LAM

--- 2Bim/AndroidToolbar/app/src/main/java/com/example/alunos/androidtoolbar/AddItemsFragment.java
+++ 2Bim/AndroidToolbar/app/src/main/java/com/example/alunos/androidtoolbar/AddItemsFragment.java
@@ -6,6 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AddItemsFragment extends Fragment {
     @Nullable
@@ -15,5 +19,13 @@ public class AddItemsFragment extends Fragment {
             View v = inflater.inflate(R.layout.fragment_add_items_layout,
                     container, false);
             return v;
+
     }
+
+    public void salvar(View v) {
+        EditText nome = findViewById(R.id.nome);
+        String oNome =  nome.getText().toString();
+    }
+    lista.add(oNome)
+
 }
--- 2Bim/AndroidToolbar/app/src/main/java/com/example/alunos/androidtoolbar/ViewItensFragment.java
+++ 2Bim/AndroidToolbar/app/src/main/java/com/example/alunos/androidtoolbar/ViewItensFragment.java
@@ -1,6 +1,28 @@
 package com.example.alunos.androidtoolbar;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
 import android.support.v4.app.Fragment;
 
 public class ViewItensFragment extends Fragment {
+    MainActivity atividade = (MainActivity) getActivity();
+
+    ListView listView = findViewById(R.id.listView);
+
+    Intent intent = getIntent();
+    Bundle bundle = intent.getExtras();
+
+        try {
+        ArrayList<Pessoa> lista = bundle.getParcelableArrayList(
+                "contatos");
+        PessoaAdapter adapter = new PessoaAdapter(
+                fragment_view_itens_layout.this, lista);
+        listView.setAdapter(adapter);
+    } catch (Exception e) {
+        Log.d(e.getClass().toString(), e.getMessage());
+    }
 }
